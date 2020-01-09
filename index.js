5eea50b2959326e09aee68a9b35f0d5530662598
Julien May <julien.may@gmail.com>
2020-01-09 23:42:20 +0100

    added async function

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const core = require('@actions/core');
 const { GitHub, context } = require('@actions/github')
 
-const github = new GitHub(process.env.GITHUB_TOKEN)
-const response = github.checks.listSuitesForRef({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    ref: context.sha,
-    app_id: 15368
-});
+async function run() {
+    try {
+        const github = await new GitHub(process.env.GITHUB_TOKEN)
+        const response = github.checks.listSuitesForRef({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            ref: context.sha,
+            app_id: 15368
+        });
 
-core.exportVariable('CHECK_SUITE_ID', response.data.check_suites[0].id)
+        console.log(JSON.stringify(response, null, 4));
+
+        core.exportVariable('CHECK_SUITE_ID', response.data.check_suites[0].id)
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
